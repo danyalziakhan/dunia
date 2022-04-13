71c4504b0ca73f8d124d6322014aa40583094cf7
Danyal Zia <danyal6870@gmail.com>
2022-04-13 06:40:05 +0500

    Add MIT license and update project configuration files

--- dunia/browser.py
+++ dunia/browser.py
@@ -1,28 +1,39 @@
+# MIT License
+
+# Copyright (c) 2022 Danyal Zia Khan
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 """
 These Protocols can be used in any case of Browser-like class (such as Playwright's BrowserContext, etc.) for static type checking as long as the implementation contains these methods
 
 Sub-classes don't need to explicitly inherit from it to be used in place of this super-class.
 """
 
-# Copyright (c) 2021-present, GYU Co., Ltd. All rights reserved.
-
-# Author: Danyal Zia Khan
-# Email: danyal6870@gmail.com
-# Copyright (c) 2020-2022 Danyal Zia Khan
-# All rights reserved.
-
-# USE OF THIS SOFTWARE AND DISTRIBUTION OUTSIDE THE GYU COMPANY IS STRICTLY PROHIBITED. PLEASE REPORT ANY SUCH USE TO THE COMPANY.
-
 from __future__ import annotations
 
 import os
-
 from dataclasses import dataclass, field
 from pathlib import Path
 from typing import TYPE_CHECKING, Any, Protocol, TypedDict, TypeVar
 
 from dunia.page import Page
-
 
 if TYPE_CHECKING:
 
