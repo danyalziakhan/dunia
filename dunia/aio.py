d4b12540a0c9e7c0152ecea41cb0993578d249ac
Danyal Zia <danyal6870@gmail.com>
2022-12-20 13:48:39 +0500

    Add upto 18 task support in gather()

--- dunia/aio.py
+++ dunia/aio.py
@@ -48,6 +48,14 @@ if TYPE_CHECKING:
     _T9 = TypeVar("_T9")
     _T10 = TypeVar("_T10")
     _T11 = TypeVar("_T11")
+    _T12 = TypeVar("_T12")
+    _T13 = TypeVar("_T13")
+    _T14 = TypeVar("_T14")
+    _T15 = TypeVar("_T15")
+    _T16 = TypeVar("_T16")
+    _T17 = TypeVar("_T17")
+    _T18 = TypeVar("_T18")
+
     _FutureT = Future[_T] | Generator[Any, None, _T] | Awaitable[_T]
 
     # ? Typing for asyncio.gather() for upto 8 coroutines
@@ -194,6 +202,242 @@ if TYPE_CHECKING:
         ...
 
     @overload
+    def gather(  # type: ignore
+        __coro_or_future1: _FutureT[_T1],
+        __coro_or_future2: _FutureT[_T2],
+        __coro_or_future3: _FutureT[_T3],
+        __coro_or_future4: _FutureT[_T4],
+        __coro_or_future5: _FutureT[_T5],
+        __coro_or_future6: _FutureT[_T6],
+        __coro_or_future7: _FutureT[_T7],
+        __coro_or_future8: _FutureT[_T8],
+        __coro_or_future9: _FutureT[_T9],
+        __coro_or_future10: _FutureT[_T10],
+        __coro_or_future11: _FutureT[_T11],
+        __coro_or_future12: _FutureT[_T12],
+        *,
+        return_exceptions: Literal[False] = ...,
+    ) -> Future[tuple[_T1, _T2, _T3, _T4, _T5, _T6, _T7, _T8, _T9, _T10, _T11, _T12]]:
+        ...
+
+    @overload
+    def gather(  # type: ignore
+        __coro_or_future1: _FutureT[_T1],
+        __coro_or_future2: _FutureT[_T2],
+        __coro_or_future3: _FutureT[_T3],
+        __coro_or_future4: _FutureT[_T4],
+        __coro_or_future5: _FutureT[_T5],
+        __coro_or_future6: _FutureT[_T6],
+        __coro_or_future7: _FutureT[_T7],
+        __coro_or_future8: _FutureT[_T8],
+        __coro_or_future9: _FutureT[_T9],
+        __coro_or_future10: _FutureT[_T10],
+        __coro_or_future11: _FutureT[_T11],
+        __coro_or_future12: _FutureT[_T12],
+        __coro_or_future13: _FutureT[_T13],
+        *,
+        return_exceptions: Literal[False] = ...,
+    ) -> Future[
+        tuple[_T1, _T2, _T3, _T4, _T5, _T6, _T7, _T8, _T9, _T10, _T11, _T12, _T13]
+    ]:
+        ...
+
+    @overload
+    def gather(  # type: ignore
+        __coro_or_future1: _FutureT[_T1],
+        __coro_or_future2: _FutureT[_T2],
+        __coro_or_future3: _FutureT[_T3],
+        __coro_or_future4: _FutureT[_T4],
+        __coro_or_future5: _FutureT[_T5],
+        __coro_or_future6: _FutureT[_T6],
+        __coro_or_future7: _FutureT[_T7],
+        __coro_or_future8: _FutureT[_T8],
+        __coro_or_future9: _FutureT[_T9],
+        __coro_or_future10: _FutureT[_T10],
+        __coro_or_future11: _FutureT[_T11],
+        __coro_or_future12: _FutureT[_T12],
+        __coro_or_future13: _FutureT[_T13],
+        __coro_or_future14: _FutureT[_T14],
+        *,
+        return_exceptions: Literal[False] = ...,
+    ) -> Future[
+        tuple[_T1, _T2, _T3, _T4, _T5, _T6, _T7, _T8, _T9, _T10, _T11, _T12, _T13, _T14]
+    ]:
+        ...
+
+    @overload
+    def gather(  # type: ignore
+        __coro_or_future1: _FutureT[_T1],
+        __coro_or_future2: _FutureT[_T2],
+        __coro_or_future3: _FutureT[_T3],
+        __coro_or_future4: _FutureT[_T4],
+        __coro_or_future5: _FutureT[_T5],
+        __coro_or_future6: _FutureT[_T6],
+        __coro_or_future7: _FutureT[_T7],
+        __coro_or_future8: _FutureT[_T8],
+        __coro_or_future9: _FutureT[_T9],
+        __coro_or_future10: _FutureT[_T10],
+        __coro_or_future11: _FutureT[_T11],
+        __coro_or_future12: _FutureT[_T12],
+        __coro_or_future13: _FutureT[_T13],
+        __coro_or_future14: _FutureT[_T14],
+        __coro_or_future15: _FutureT[_T15],
+        *,
+        return_exceptions: Literal[False] = ...,
+    ) -> Future[
+        tuple[
+            _T1,
+            _T2,
+            _T3,
+            _T4,
+            _T5,
+            _T6,
+            _T7,
+            _T8,
+            _T9,
+            _T10,
+            _T11,
+            _T12,
+            _T13,
+            _T14,
+            _T15,
+        ]
+    ]:
+        ...
+
+    @overload
+    def gather(  # type: ignore
+        __coro_or_future1: _FutureT[_T1],
+        __coro_or_future2: _FutureT[_T2],
+        __coro_or_future3: _FutureT[_T3],
+        __coro_or_future4: _FutureT[_T4],
+        __coro_or_future5: _FutureT[_T5],
+        __coro_or_future6: _FutureT[_T6],
+        __coro_or_future7: _FutureT[_T7],
+        __coro_or_future8: _FutureT[_T8],
+        __coro_or_future9: _FutureT[_T9],
+        __coro_or_future10: _FutureT[_T10],
+        __coro_or_future11: _FutureT[_T11],
+        __coro_or_future12: _FutureT[_T12],
+        __coro_or_future13: _FutureT[_T13],
+        __coro_or_future14: _FutureT[_T14],
+        __coro_or_future15: _FutureT[_T15],
+        __coro_or_future16: _FutureT[_T16],
+        *,
+        return_exceptions: Literal[False] = ...,
+    ) -> Future[
+        tuple[
+            _T1,
+            _T2,
+            _T3,
+            _T4,
+            _T5,
+            _T6,
+            _T7,
+            _T8,
+            _T9,
+            _T10,
+            _T11,
+            _T12,
+            _T13,
+            _T14,
+            _T15,
+            _T16,
+        ]
+    ]:
+        ...
+
+    @overload
+    def gather(  # type: ignore
+        __coro_or_future1: _FutureT[_T1],
+        __coro_or_future2: _FutureT[_T2],
+        __coro_or_future3: _FutureT[_T3],
+        __coro_or_future4: _FutureT[_T4],
+        __coro_or_future5: _FutureT[_T5],
+        __coro_or_future6: _FutureT[_T6],
+        __coro_or_future7: _FutureT[_T7],
+        __coro_or_future8: _FutureT[_T8],
+        __coro_or_future9: _FutureT[_T9],
+        __coro_or_future10: _FutureT[_T10],
+        __coro_or_future11: _FutureT[_T11],
+        __coro_or_future12: _FutureT[_T12],
+        __coro_or_future13: _FutureT[_T13],
+        __coro_or_future14: _FutureT[_T14],
+        __coro_or_future15: _FutureT[_T15],
+        __coro_or_future16: _FutureT[_T16],
+        __coro_or_future17: _FutureT[_T17],
+        *,
+        return_exceptions: Literal[False] = ...,
+    ) -> Future[
+        tuple[
+            _T1,
+            _T2,
+            _T3,
+            _T4,
+            _T5,
+            _T6,
+            _T7,
+            _T8,
+            _T9,
+            _T10,
+            _T11,
+            _T12,
+            _T13,
+            _T14,
+            _T15,
+            _T16,
+            _T17,
+        ]
+    ]:
+        ...
+
+    @overload
+    def gather(  # type: ignore
+        __coro_or_future1: _FutureT[_T1],
+        __coro_or_future2: _FutureT[_T2],
+        __coro_or_future3: _FutureT[_T3],
+        __coro_or_future4: _FutureT[_T4],
+        __coro_or_future5: _FutureT[_T5],
+        __coro_or_future6: _FutureT[_T6],
+        __coro_or_future7: _FutureT[_T7],
+        __coro_or_future8: _FutureT[_T8],
+        __coro_or_future9: _FutureT[_T9],
+        __coro_or_future10: _FutureT[_T10],
+        __coro_or_future11: _FutureT[_T11],
+        __coro_or_future12: _FutureT[_T12],
+        __coro_or_future13: _FutureT[_T13],
+        __coro_or_future14: _FutureT[_T14],
+        __coro_or_future15: _FutureT[_T15],
+        __coro_or_future16: _FutureT[_T16],
+        __coro_or_future17: _FutureT[_T17],
+        __coro_or_future18: _FutureT[_T18],
+        *,
+        return_exceptions: Literal[False] = ...,
+    ) -> Future[
+        tuple[
+            _T1,
+            _T2,
+            _T3,
+            _T4,
+            _T5,
+            _T6,
+            _T7,
+            _T8,
+            _T9,
+            _T10,
+            _T11,
+            _T12,
+            _T13,
+            _T14,
+            _T15,
+            _T16,
+            _T17,
+            _T18,
+        ]
+    ]:
+        ...
+
+    @overload
     def gather(
         __coro_or_future1: _FutureT[Any],
         __coro_or_future2: _FutureT[Any],
@@ -207,6 +451,13 @@ if TYPE_CHECKING:
         __coro_or_future10: _FutureT[Any],
         __coro_or_future11: _FutureT[Any],
         __coro_or_future12: _FutureT[Any],
+        __coro_or_future13: _FutureT[Any],
+        __coro_or_future14: _FutureT[Any],
+        __coro_or_future15: _FutureT[Any],
+        __coro_or_future16: _FutureT[Any],
+        __coro_or_future17: _FutureT[Any],
+        __coro_or_future18: _FutureT[Any],
+        __coro_or_future19: _FutureT[Any],
         *coros_or_futures: _FutureT[Any],
         return_exceptions: bool = ...,
     ) -> Future[list[Any]]:
@@ -433,6 +684,286 @@ if TYPE_CHECKING:
             _T9 | BaseException,
             _T10 | BaseException,
             _T11 | BaseException,
+        ]
+    ]:
+        ...
+
+    @overload
+    def gather(
+        __coro_or_future1: _FutureT[_T1],
+        __coro_or_future2: _FutureT[_T2],
+        __coro_or_future3: _FutureT[_T3],
+        __coro_or_future4: _FutureT[_T4],
+        __coro_or_future5: _FutureT[_T5],
+        __coro_or_future6: _FutureT[_T6],
+        __coro_or_future7: _FutureT[_T7],
+        __coro_or_future8: _FutureT[_T8],
+        __coro_or_future9: _FutureT[_T9],
+        __coro_or_future10: _FutureT[_T10],
+        __coro_or_future11: _FutureT[_T11],
+        __coro_or_future12: _FutureT[_T12],
+        *,
+        return_exceptions: bool = ...,
+    ) -> Future[
+        tuple[
+            _T1 | BaseException,
+            _T2 | BaseException,
+            _T3 | BaseException,
+            _T4 | BaseException,
+            _T5 | BaseException,
+            _T6 | BaseException,
+            _T7 | BaseException,
+            _T8 | BaseException,
+            _T9 | BaseException,
+            _T10 | BaseException,
+            _T11 | BaseException,
+            _T12 | BaseException,
+        ]
+    ]:
+        ...
+
+    @overload
+    def gather(
+        __coro_or_future1: _FutureT[_T1],
+        __coro_or_future2: _FutureT[_T2],
+        __coro_or_future3: _FutureT[_T3],
+        __coro_or_future4: _FutureT[_T4],
+        __coro_or_future5: _FutureT[_T5],
+        __coro_or_future6: _FutureT[_T6],
+        __coro_or_future7: _FutureT[_T7],
+        __coro_or_future8: _FutureT[_T8],
+        __coro_or_future9: _FutureT[_T9],
+        __coro_or_future10: _FutureT[_T10],
+        __coro_or_future11: _FutureT[_T11],
+        __coro_or_future12: _FutureT[_T12],
+        __coro_or_future13: _FutureT[_T13],
+        *,
+        return_exceptions: bool = ...,
+    ) -> Future[
+        tuple[
+            _T1 | BaseException,
+            _T2 | BaseException,
+            _T3 | BaseException,
+            _T4 | BaseException,
+            _T5 | BaseException,
+            _T6 | BaseException,
+            _T7 | BaseException,
+            _T8 | BaseException,
+            _T9 | BaseException,
+            _T10 | BaseException,
+            _T11 | BaseException,
+            _T12 | BaseException,
+            _T13 | BaseException,
+        ]
+    ]:
+        ...
+
+    @overload
+    def gather(
+        __coro_or_future1: _FutureT[_T1],
+        __coro_or_future2: _FutureT[_T2],
+        __coro_or_future3: _FutureT[_T3],
+        __coro_or_future4: _FutureT[_T4],
+        __coro_or_future5: _FutureT[_T5],
+        __coro_or_future6: _FutureT[_T6],
+        __coro_or_future7: _FutureT[_T7],
+        __coro_or_future8: _FutureT[_T8],
+        __coro_or_future9: _FutureT[_T9],
+        __coro_or_future10: _FutureT[_T10],
+        __coro_or_future11: _FutureT[_T11],
+        __coro_or_future12: _FutureT[_T12],
+        __coro_or_future13: _FutureT[_T13],
+        __coro_or_future14: _FutureT[_T14],
+        *,
+        return_exceptions: bool = ...,
+    ) -> Future[
+        tuple[
+            _T1 | BaseException,
+            _T2 | BaseException,
+            _T3 | BaseException,
+            _T4 | BaseException,
+            _T5 | BaseException,
+            _T6 | BaseException,
+            _T7 | BaseException,
+            _T8 | BaseException,
+            _T9 | BaseException,
+            _T10 | BaseException,
+            _T11 | BaseException,
+            _T12 | BaseException,
+            _T13 | BaseException,
+            _T14 | BaseException,
+        ]
+    ]:
+        ...
+
+    @overload
+    def gather(
+        __coro_or_future1: _FutureT[_T1],
+        __coro_or_future2: _FutureT[_T2],
+        __coro_or_future3: _FutureT[_T3],
+        __coro_or_future4: _FutureT[_T4],
+        __coro_or_future5: _FutureT[_T5],
+        __coro_or_future6: _FutureT[_T6],
+        __coro_or_future7: _FutureT[_T7],
+        __coro_or_future8: _FutureT[_T8],
+        __coro_or_future9: _FutureT[_T9],
+        __coro_or_future10: _FutureT[_T10],
+        __coro_or_future11: _FutureT[_T11],
+        __coro_or_future12: _FutureT[_T12],
+        __coro_or_future13: _FutureT[_T13],
+        __coro_or_future14: _FutureT[_T14],
+        __coro_or_future15: _FutureT[_T15],
+        *,
+        return_exceptions: bool = ...,
+    ) -> Future[
+        tuple[
+            _T1 | BaseException,
+            _T2 | BaseException,
+            _T3 | BaseException,
+            _T4 | BaseException,
+            _T5 | BaseException,
+            _T6 | BaseException,
+            _T7 | BaseException,
+            _T8 | BaseException,
+            _T9 | BaseException,
+            _T10 | BaseException,
+            _T11 | BaseException,
+            _T12 | BaseException,
+            _T13 | BaseException,
+            _T14 | BaseException,
+            _T15 | BaseException,
+        ]
+    ]:
+        ...
+
+    @overload
+    def gather(
+        __coro_or_future1: _FutureT[_T1],
+        __coro_or_future2: _FutureT[_T2],
+        __coro_or_future3: _FutureT[_T3],
+        __coro_or_future4: _FutureT[_T4],
+        __coro_or_future5: _FutureT[_T5],
+        __coro_or_future6: _FutureT[_T6],
+        __coro_or_future7: _FutureT[_T7],
+        __coro_or_future8: _FutureT[_T8],
+        __coro_or_future9: _FutureT[_T9],
+        __coro_or_future10: _FutureT[_T10],
+        __coro_or_future11: _FutureT[_T11],
+        __coro_or_future12: _FutureT[_T12],
+        __coro_or_future13: _FutureT[_T13],
+        __coro_or_future14: _FutureT[_T14],
+        __coro_or_future15: _FutureT[_T15],
+        __coro_or_future16: _FutureT[_T16],
+        *,
+        return_exceptions: bool = ...,
+    ) -> Future[
+        tuple[
+            _T1 | BaseException,
+            _T2 | BaseException,
+            _T3 | BaseException,
+            _T4 | BaseException,
+            _T5 | BaseException,
+            _T6 | BaseException,
+            _T7 | BaseException,
+            _T8 | BaseException,
+            _T9 | BaseException,
+            _T10 | BaseException,
+            _T11 | BaseException,
+            _T12 | BaseException,
+            _T13 | BaseException,
+            _T14 | BaseException,
+            _T15 | BaseException,
+            _T16 | BaseException,
+        ]
+    ]:
+        ...
+
+    @overload
+    def gather(
+        __coro_or_future1: _FutureT[_T1],
+        __coro_or_future2: _FutureT[_T2],
+        __coro_or_future3: _FutureT[_T3],
+        __coro_or_future4: _FutureT[_T4],
+        __coro_or_future5: _FutureT[_T5],
+        __coro_or_future6: _FutureT[_T6],
+        __coro_or_future7: _FutureT[_T7],
+        __coro_or_future8: _FutureT[_T8],
+        __coro_or_future9: _FutureT[_T9],
+        __coro_or_future10: _FutureT[_T10],
+        __coro_or_future11: _FutureT[_T11],
+        __coro_or_future12: _FutureT[_T12],
+        __coro_or_future13: _FutureT[_T13],
+        __coro_or_future14: _FutureT[_T14],
+        __coro_or_future15: _FutureT[_T15],
+        __coro_or_future16: _FutureT[_T16],
+        __coro_or_future17: _FutureT[_T17],
+        *,
+        return_exceptions: bool = ...,
+    ) -> Future[
+        tuple[
+            _T1 | BaseException,
+            _T2 | BaseException,
+            _T3 | BaseException,
+            _T4 | BaseException,
+            _T5 | BaseException,
+            _T6 | BaseException,
+            _T7 | BaseException,
+            _T8 | BaseException,
+            _T9 | BaseException,
+            _T10 | BaseException,
+            _T11 | BaseException,
+            _T12 | BaseException,
+            _T13 | BaseException,
+            _T14 | BaseException,
+            _T15 | BaseException,
+            _T16 | BaseException,
+            _T17 | BaseException,
+        ]
+    ]:
+        ...
+
+    @overload
+    def gather(
+        __coro_or_future1: _FutureT[_T1],
+        __coro_or_future2: _FutureT[_T2],
+        __coro_or_future3: _FutureT[_T3],
+        __coro_or_future4: _FutureT[_T4],
+        __coro_or_future5: _FutureT[_T5],
+        __coro_or_future6: _FutureT[_T6],
+        __coro_or_future7: _FutureT[_T7],
+        __coro_or_future8: _FutureT[_T8],
+        __coro_or_future9: _FutureT[_T9],
+        __coro_or_future10: _FutureT[_T10],
+        __coro_or_future11: _FutureT[_T11],
+        __coro_or_future12: _FutureT[_T12],
+        __coro_or_future13: _FutureT[_T13],
+        __coro_or_future14: _FutureT[_T14],
+        __coro_or_future15: _FutureT[_T15],
+        __coro_or_future16: _FutureT[_T16],
+        __coro_or_future17: _FutureT[_T17],
+        __coro_or_future18: _FutureT[_T18],
+        *,
+        return_exceptions: bool = ...,
+    ) -> Future[
+        tuple[
+            _T1 | BaseException,
+            _T2 | BaseException,
+            _T3 | BaseException,
+            _T4 | BaseException,
+            _T5 | BaseException,
+            _T6 | BaseException,
+            _T7 | BaseException,
+            _T8 | BaseException,
+            _T9 | BaseException,
+            _T10 | BaseException,
+            _T11 | BaseException,
+            _T12 | BaseException,
+            _T13 | BaseException,
+            _T14 | BaseException,
+            _T15 | BaseException,
+            _T16 | BaseException,
+            _T17 | BaseException,
+            _T18 | BaseException,
         ]
     ]:
         ...
